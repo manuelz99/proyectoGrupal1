339223a2bb8e37cb8205d955d15f214733f3b096
Manuel Zapotocznyj <zapotocznyjmanuel@outlook.com>
2026-04-30 03:40:35 -0300

    Anotaciones en clases base Pedido , plato y usuario + creacion enum EstadoPedido

--- proyecto/src/main/java/grupo/proyecto/Models/Restaurante.java
+++ proyecto/src/main/java/grupo/proyecto/Models/Restaurante.java
@@ -1,15 +1,28 @@
 package grupo.proyecto.Models;
 
+import jakarta.persistence.*;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.repository.NoRepositoryBean;
 
 import java.util.List;
+@Entity
+@Table(name = "restaurantes")
+@NoArgsConstructor
 @Data
 public class Restaurante {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+        @Column(nullable = false)
     private String nombre;
-    private String especialidad; // Ej: "Sushi", "Parrilla"
+    @Column(nullable = false)
+    private String especialidad;
+    @Column(nullable = false)// Ej: "Sushi", "Parrilla"
     private Double calificacion;
+    @OneToMany(mappedBy = "restaurante",cascade = CascadeType.ALL,fetch = FetchType.LAZY)
     private List<Plato> menu;
+
     private boolean ofreceOpcionesSaludables;
 
     // Constructores, Getters y Setters
