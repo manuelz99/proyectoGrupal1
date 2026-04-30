cb39d5f83012ce3ba5dc273d1316a1f7e6d8a82f
Manuel Zapotocznyj <zapotocznyjmanuel@outlook.com>
2026-04-30 03:17:11 -0300

    anotaciones en "plato"

--- proyecto/src/main/java/grupo/proyecto/Models/Plato.java
+++ proyecto/src/main/java/grupo/proyecto/Models/Plato.java
@@ -19,8 +19,10 @@ public class Plato {
     private String descripcion;
     @Column(nullable = false)
     private Double precio;
-    @OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
-    private List<String> etiquetas; // Ej: "Keto", "Sin Azúcar", "Proteico"
+    @ElementCollection(fetch = FetchType.EAGER) // Correcto para listas de tipos simples
+    @CollectionTable(name = "plato_etiquetas", joinColumns = @JoinColumn(name = "plato_id"))
+    @Column(name = "etiqueta")
+    private List<String> etiquetas; // Ej: "Keto", "Sin Azúcar", "Vegano"
 
     private boolean disponible=true;
 
