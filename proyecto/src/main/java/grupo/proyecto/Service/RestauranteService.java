8242f3944ccebfade5c115f4f734f30ad50e753f
francesoAntonio-Guerra <tifrancescoguerra@gmail.com>
2026-05-24 13:58:19 -0300

    Implemento RestaurantMapper y actualizo service/controller

--- proyecto/src/main/java/grupo/proyecto/Service/RestauranteService.java
+++ proyecto/src/main/java/grupo/proyecto/Service/RestauranteService.java
@@ -1,5 +1,6 @@
 package grupo.proyecto.Service;
 
+import grupo.proyecto.Mapper.RestauranteMapper;
 import grupo.proyecto.Models.Restaurante;
 import grupo.proyecto.Models.dto.request.RestauranteRequestDTO;
 import grupo.proyecto.Models.dto.response.RestauranteResponseDTO;
@@ -15,14 +16,14 @@ import java.util.List;
 public class RestauranteService {
 
     private final RestauranteRepository repository;
-    //private final RestauranteMapper mapper;
+    private final RestauranteMapper mapper;
 
     public RestauranteResponseDTO crearRestaurante(RestauranteRequestDTO dto) {
 
-        //Restaurante restaurante = mapper.toEntity(dto);
-        //Restaurante saved = repository.save(restaurante);
+        Restaurante restaurante = mapper.toEntity(dto);
+        Restaurante saved = repository.save(restaurante);
 
-        return /*mapper.toDTO(saved)*/ new RestauranteResponseDTO();
+        return mapper.toDTO(saved);
     }
 
     public RestauranteResponseDTO buscarPorId(Long id) {
@@ -30,16 +31,26 @@ public class RestauranteService {
         Restaurante restaurante = repository.findById(id)
                 .orElseThrow(() -> new RecursoNotFoundException("Restaurante no encontrado con ID: " + id));
 
-        return /*mapper.toDTO(restaurante)*/ new RestauranteResponseDTO();
+        return mapper.toDTO(restaurante);
     }
 
-    /*
-    public List<RestauranteResponseDTO> listar() {
+    public List<RestauranteResponseDTO> listar(String nombre, String direccion) {
 
-        return repository.findAll()
-                .stream()
+        List<Restaurante> restaurantes;
+
+        if (nombre != null) {
+
+            restaurantes = repository.findByNombre(nombre);
+        } else if (direccion != null){
+
+            restaurantes = repository.findByDireccion(direccion);
+        } else {
+
+            restaurantes = repository.findAll();
+        }
+
+        return restaurantes.stream()
                 .map(mapper::toDTO)
                 .toList();
     }
-     */
 }
