2b9ad6cbe3e81080a12702e6f6c8b9b4225359c9
FrancescoGuerra <tifrancescoguerra@gmail.com>
2026-06-18 14:15:37 -0300

    Agrego clase SwaggerConfig

---
+++ proyecto/src/main/java/grupo/proyecto/config/SwaggerConfig.java
@@ -0,0 +1,32 @@
+package grupo.proyecto.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        final String securitySchemeName = "bearerAuth";
+
+        return new OpenAPI()
+                // Esto aplica el candadito a todos los endpoints automáticamente
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+                // Acá configuramos el botón verde para que acepte JWT
+                .components(
+                        new Components()
+                                .addSecuritySchemes(securitySchemeName,
+                                        new SecurityScheme()
+                                                .name(securitySchemeName)
+                                                .type(SecurityScheme.Type.HTTP)
+                                                .scheme("bearer")
+                                                .bearerFormat("JWT")
+                                )
+                );
+    }
+}
